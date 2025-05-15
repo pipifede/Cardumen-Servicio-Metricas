6eb2a67b6abd0f53f5e21a1a6d0f5f435d219861
SantiagoCastano03 <santiagocasmdp@gmail.com>
2025-05-15 18:22:49 -0300

    2 modelos agregados

--- app/config.py
+++ app/config.py
@@ -14,4 +14,4 @@ os.makedirs(YOLO_MODEL_PATH, exist_ok=True)
 os.makedirs(MEDIAPIPE_MODEL_PATH, exist_ok=True)
 
 MODELOSYOLO = ['yolo11n.pt', 'yolo11s.pt', 'yolo11m.pt', 'yolo11l.pt', 'yolo11x.pt']
-MODELOS_MEDIAPIPE = ["efficientdet_lite0_pf32.tflite","efficientdet_lite0_int8.tflite", "efficientdet_lite0_pf16.tflite", "ssd_mobilenet_v2_int8.tflite", "ssd_mobilenet_v2_pf32.tflite" ]
+MODELOS_MEDIAPIPE = ["efficientdet_lite0_pf32.tflite","efficientdet_lite0_int8.tflite", "efficientdet_lite0_pf16.tflite", "ssd_mobilenet_v2_int8.tflite", "ssd_mobilenet_v2_pf32.tflite", "efficientdet_lite2_pf32.tflite", "efficientdet_lite2_int8.tflite"]
