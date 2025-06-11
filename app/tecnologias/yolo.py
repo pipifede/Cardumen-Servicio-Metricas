ea56d5bb2fd6f1883ee8a6a17c1e142b0d12c63c
Nicolas Dartayeta <nicodarta1305@gmail.com>
2025-06-11 11:14:23 -0300

    boxes guardadas cuando se procesa un video

--- app/tecnologias/yolo.py
+++ app/tecnologias/yolo.py
@@ -71,7 +71,7 @@ class YOLOModel:
         # Actualizar métricas
         self.update_frame_metrics(inference_time, processing_time, confidences)
         
-        return processed_frame
+        return {"processed_frame": processed_frame, "results": results}
 
     def get_current_metrics(self):
         """Devuelve las métricas calculadas hasta el momento"""
